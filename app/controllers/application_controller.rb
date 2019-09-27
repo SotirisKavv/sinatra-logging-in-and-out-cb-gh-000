401598b714b3bdd89d5fabcd46ac759f5b00efe2
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-27 20:38:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/account'
     end
-      erb :error
-    end
+    erb :error
   end
 
   get '/account' do
